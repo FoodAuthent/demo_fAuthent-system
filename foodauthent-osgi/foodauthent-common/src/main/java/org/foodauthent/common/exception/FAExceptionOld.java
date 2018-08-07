0e41f77d9c7021b4f5bdfa5ace671b6b99e9832f
Alexander Kerner <alexander.kerner@lablicate.com>
2018-08-07 12:30:37 +0200

    Rename old Exception

---
+++ foodauthent-osgi/foodauthent-common/src/main/java/org/foodauthent/common/exception/FAExceptionOld.java
@@ -0,0 +1,35 @@
+package org.foodauthent.common.exception;
+
+public class FAExceptionOld extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1566059953084078199L;
+
+    public FAExceptionOld() {
+
+    }
+
+    public FAExceptionOld(final String message) {
+	super(message);
+
+    }
+
+    public FAExceptionOld(final String message, final Throwable cause) {
+	super(message, cause);
+
+    }
+
+    public FAExceptionOld(final String message, final Throwable cause, final boolean enableSuppression,
+	    final boolean writableStackTrace) {
+	super(message, cause, enableSuppression, writableStackTrace);
+
+    }
+
+    public FAExceptionOld(final Throwable cause) {
+	super(cause);
+
+    }
+
+}
