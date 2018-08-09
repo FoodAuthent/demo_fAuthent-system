2c37e188ca4fcc0e22c859a69c8242a5fd7797a2
Alexander Kerner <alexander.kerner@lablicate.com>
2018-08-09 13:14:49 +0200

    Testing

--- foodauthent-osgi/foodauthent-test/src/test/org/foodauthent/test/ServiceReferenceTest.java
+++ foodauthent-osgi/foodauthent-test/src/test/org/foodauthent/test/ServiceReferenceTest.java
@@ -1,5 +1,10 @@
 package org.foodauthent.test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
 import java.io.File;
 import java.util.UUID;
 
@@ -39,11 +44,12 @@ public class ServiceReferenceTest extends AbstractITTest {
 	Response response = TestUtils.uploadFileData(wt, fileMetaID, new File("files/bruker-nmr/1.zip"));
 	BundleContext ctx = FrameworkUtil.getBundle(RawFileReader.class).getBundleContext();
 	for (ServiceReference<?> sr : ctx.getAllServiceReferences(null, null)) {
-	    System.err.println(sr);
+	    // System.err.println(sr);
 	}
-
-	RawFileReader reader = ctx.getService(ctx.getServiceReference(RawFileReader.class));
-	System.err.println(reader);
+	ServiceReference<RawFileReader> hans = ctx.getServiceReference(RawFileReader.class);
+	assertThat(hans, is(not(nullValue())));
+	RawFileReader reader = ctx.getService(hans);
+	assertThat(reader, is(not(nullValue())));
 
 
     }
