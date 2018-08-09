64fb6b0c59d90e827edeb3c7fb076b2828742002
Martin Horn <martin.horn@knime.com>
2018-08-09 17:42:41 +0200

    change relative path in js-client model template

--- foodauthent-osgi/foodauthent-ui-vuejs/src/generated/rest-client/src/model/TrainingWorkflowInput.js
+++ foodauthent-osgi/foodauthent-ui-vuejs/src/generated/rest-client/src/model/TrainingWorkflowInput.js
@@ -17,7 +17,7 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['ApiClient', 'model/FingerprintSet', 'model/WorkflowModuleInput', 'model/WorkflowParameter'], factory);
+    define(['../ApiClient', '../model/FingerprintSet', '../model/WorkflowModuleInput', '../model/WorkflowParameter'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // CommonJS-like environments that support module.exports, like Node.
     module.exports = factory(require('../ApiClient'), require('./FingerprintSet'), require('./WorkflowModuleInput'), require('./WorkflowParameter'));
