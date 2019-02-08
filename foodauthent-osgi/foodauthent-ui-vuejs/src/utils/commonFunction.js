0cdf432e8db3922f0a40e3dd03b6cfc1fbb80300
Bevilacqua, Massimo <mbevilacqua@benelog.com>
2019-02-08 11:03:29 +0100

    nice vue

--- foodauthent-osgi/foodauthent-ui-vuejs/src/utils/commonFunction.js
+++ foodauthent-osgi/foodauthent-ui-vuejs/src/utils/commonFunction.js
@@ -10,6 +10,13 @@ var MyObject = function () {
 	  var customMetadataApi = new CustomMetadataApi(apiClient);
 
 	  var getModelSchemas = function(modelID,schemas,schemaIdHolder){
+		  console.log("Schema at begin",schemas);
+		  if (typeof schemas !== 'undefined' && schemas.length > 0) {
+			  console.log("Schema inside if",schemas);
+//			  schemas.splice(0,schemas.length);
+			  schemas = [];
+			}
+		  console.log("Schema outside if",schemas);
 		  var getCustomMetadataSchemasCallback = function (error, data, response) {
 			  if (error) {
 		        console.error("getCustomMetadataSchemasCallback error",error);
@@ -42,18 +49,18 @@ var MyObject = function () {
 		    	        		        for (var fieldsindex = 0; fieldsindex < fieldslength; fieldsindex++) {
 		    	        		        		// TODO make Field JSON element
 		    	        		        		var currentField = fields[fieldsKeys[fieldsindex]];
-		    	        		        		// console.log(currentField);
+		    	        		        		console.log("nestedSchemaKeys",nestedSchemaKeys[nestedindex]);
 		    	        		        		var UIField ;
-		    	        		        		switch(currentField.type){
-		    	        		        		case 'string':
+//		    	        		        		switch(currentField.type){
+//		    	        		        		case 'string':
 		    	        		        			UIField= {
-			    	        		        		    "label" : currentField.title,
-			    	        		        		    "model" : currentField.$id.substring(currentField.$id.lastIndexOf("/")+1,currentField.$id.length),
+			    	        		        		    "label" : fieldsKeys[fieldsindex],
+			    	        		        		    "model" : fieldsKeys[fieldsindex],
 			    	        		        		    "required" : false,
 			    	        		        		    "type" : "input",
 			    	        		        		    "inputType" : "text"
 			    	        		        		  }
-		    	        		        		}
+		    	        		        		//}
 		    	        		        		newUISchema["fields"].push(UIField);
 		    	        		        		newUISchema["title"] = currentSchema.title;
 		    	        		        		newUISchema["model"] = {};
@@ -62,22 +69,18 @@ var MyObject = function () {
 		    	        		        		newUISchema["key"] = nestedSchemaKeys[nestedindex]
 		    	        		        		
 		    	        		        }
-		    	        		        
+		    	        		        console.log("schemas before push",schemas);
 		    	        		        schemas.push(newUISchema);
 		        		        }
 		        		        
 		        		        
 		        		      }
 		        		  }
-		        		console.log("before if",schemaID);
 	        		if(schemaIdHolder ==undefined || schemaIdHolder.schemaID != "withOutSchema"){
-		        			console.log("I am inside");
 		        		customMetadataApi.getCustomMetadataSchema(modelID,schemaID,getCustomMetadataSchemaCallback);
 		        		}else{
 		        			schemaIdHolder.schemaID = schemaID;
-		        			console.log("else",schemaIdHolder);
 		        		}
-		        		console.log("after if",schemaID,schemaIdHolder);
 		        }
 		        
 		        
