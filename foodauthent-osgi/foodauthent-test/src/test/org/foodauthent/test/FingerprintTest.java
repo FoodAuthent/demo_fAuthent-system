230f2a4bcb6386422e744d0206fa0a3dbdfa22d3
Martin Horn <martin.horn@knime.com>
2019-02-08 12:00:05 +0100

    test added to save and retrieve fingerprintset metadata

---
+++ foodauthent-osgi/foodauthent-test/src/test/org/foodauthent/test/FingerprintTest.java
@@ -0,0 +1,41 @@
+package org.foodauthent.test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import org.foodauthent.model.Fingerprint;
+import org.foodauthent.model.FingerprintSet;
+import org.foodauthent.model.FingerprintSetPageResult;
+import org.foodauthent.rest.api.service.FingerprintRestService;
+import org.hamcrest.core.IsCollectionContaining;
+import org.junit.Test;
+
+/**
+ * Tests endpoints that deal with fingerprints and fingerprint sets.
+ */
+public class FingerprintTest extends AbstractITTest {
+
+    @Test
+    public void testSaveAndRetrieveFingerprintSetMetadata() {
+	Fingerprint fp = Fingerprint.builder().setMetadata("fp metadata").build();
+	FingerprintSet fps = FingerprintSet.builder().setName("myset").setProductId(UUID.randomUUID())
+		.setFingerprints(Arrays.asList(fp)).setFileId(UUID.randomUUID()).build();
+
+	FingerprintRestService fingerprintService = restService(FingerprintRestService.class);
+	fingerprintService.createFingerprintSet(fps);
+
+	FingerprintSet result = fingerprintService.getFingerprintSetById(fps.getFaId())
+		.readEntity(FingerprintSet.class);
+	assertThat("Unexpected fingerprint set", fps, is(result));
+
+	FingerprintSetPageResult resultPage = fingerprintService.findFingerprintSetByKeyword(1, 100, asList("myset"))
+		.readEntity(FingerprintSetPageResult.class);
+	assertThat("Fingerprint set not contained in result", resultPage.getResults(), hasItem(fps));
+    }
+
+}
