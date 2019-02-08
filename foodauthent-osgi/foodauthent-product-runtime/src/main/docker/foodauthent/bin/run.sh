5009774b412c08eab5bf50cb0b9451bb9991e861
Sven Boeckelmann <sven@boeckelmann.org>
2019-02-08 14:05:21 +0100

    re-enable maven tycho build fix docker product builds

--- foodauthent-osgi/foodauthent-product-runtime/src/main/docker/foodauthent/bin/run.sh
+++ foodauthent-osgi/foodauthent-product-runtime/src/main/docker/foodauthent/bin/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/bin/java -Xms128m -Xmx2g -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dorg.osgi.service.http.port=9090 \
- -Dorg.eclipse.equinox.http.jetty.http.host=0.0.0.0 -Dosgi.console=9023 \
+ -Dorg.eclipse.equinox.http.jetty.http.host=0.0.0.0 -Dosgi.console=9023 -Dconfig.file=/config/fa.conf \
   -jar /opt/foodauthent/runtime/plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar
