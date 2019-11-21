357b34c56236d3782603d896163b0ef6521bd508
Sven Böckelmann <sboeckelmann@benelog.com>
2019-11-21 20:38:45 +0100

    use 8-jdk-stretch as Docker container base image start xvfb virtual display and run non-headless

--- foodauthent-osgi/foodauthent-product-runtime/src/main/docker/foodauthent/bin/run.sh
+++ foodauthent-osgi/foodauthent-product-runtime/src/main/docker/foodauthent/bin/run.sh
@@ -11,6 +11,15 @@ case $DEBUG in [yY]|[yY][eE][sS]|[oO][nN]|1|[tT][rR][uU][eE])
 	RUNTIME_OPTS="${RUNTIME_OPTS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=9000" ;; 
 esac
 
-/usr/bin/java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dorg.osgi.service.http.port=9090 \
- -Dorg.eclipse.equinox.http.jetty.http.host=0.0.0.0 -Djava.awt.headless=true -Dosgi.console=9023 -Dconfig.file=/config/fa.conf \
+# run headless
+#/usr/local/openjdk-8/bin/java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dorg.osgi.service.http.port=9090 \
+# -Dorg.eclipse.equinox.http.jetty.http.host=0.0.0.0 -Djava.awt.headless=true -Dosgi.console=9023 -Dconfig.file=/config/fa.conf \
+# ${RUNTIME_OPTS} -jar /opt/foodauthent/runtime/plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar
+
+# run with display
+/usr/bin/Xvfb :1 -screen 0 1024x768x16 &> xvfb.log  &
+export DISPLAY=:1.0
+/usr/local/openjdk-8/bin/java -Declipse.ignoreApp=true -Dosgi.noShutdown=true -Dorg.osgi.service.http.port=9090 \
+ -Dorg.eclipse.equinox.http.jetty.http.host=0.0.0.0 -Dosgi.console=9023 -Dconfig.file=/config/fa.conf \
  ${RUNTIME_OPTS} -jar /opt/foodauthent/runtime/plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar
+ 
